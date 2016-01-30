a88f95b7c7d6d3a4918ec6238377a403bd1c6ac4
makkareru <makkareru@gmail.com>
2016-01-31 05:32:43 +0900

    make player make page

--- app/controllers/admin/players_controller.rb
+++ app/controllers/admin/players_controller.rb
@@ -1,13 +1,27 @@
 class Admin::PlayersController < Admin::BaseController
   def index
+    @players = Player.all
   end
 
   def new
+    @player = Player.new
   end
 
   def create
+    @player = Player.new(player_params)
+    unless @player.save
+      render :new
+    end
+    redirect_to admin_players_path
   end
 
   def edit
+    @player = Player.find(params[:id])
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name,:weight,:profile,:ranking)
   end
 end
